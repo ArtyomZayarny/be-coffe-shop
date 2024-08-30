1d8d43195dd793b3733b7ec8c5909c4c1cc884de
ArtemZayarny <artyom.zayarny@gmail.com>
2024-08-30 16:19:02 +0300

    Added logger module and validation types for product model

--- apps/products/src/dto/create-product.dto.ts
+++ apps/products/src/dto/create-product.dto.ts
@@ -1,9 +1,18 @@
-export class CreateProductDto {
-  brandId: string;
+import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
 
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
 
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  photo: string;
 }
