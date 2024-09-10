ba03b8bdd84121540dc06c98a83adb04df3c7fe6
ArtemZayarny <artyom.zayarny@gmail.com>
2024-09-10 19:06:45 +0300

    Added jwt strategy

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(user: UserDocument, response: Response) {
+    //Create token payload
+    const tokenPayload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    //set exp date for token
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    // Generate token
+    const token = this.jwtService.sign(tokenPayload);
+
+    console.log('token', token);
+    console.log('expires', expires);
+    //Set token to cookie
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    });
   }
 }
--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ProductsModule } from './products.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const configService = await app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
